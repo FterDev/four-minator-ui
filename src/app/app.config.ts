fdca6dce00b61573c2c9d9d6067927769f362223
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-02-06 15:40:37 +0100

    Added  Angular Fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,29 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideAnimationsAsync(), 
+    provideFirebaseApp(() => initializeApp({ 
+      projectId: environment.projectId, 
+      appId: environment.appId, 
+      storageBucket: environment.storageBucket, 
+      apiKey: environment.apiKey, 
+      authDomain: environment.authDomain, 
+      messagingSenderId: environment.messagingSenderId, 
+      measurementId: environment.measurementId 
+    })), 
+    provideAuth(() => getAuth()), 
+    provideAnalytics(() => getAnalytics()), 
+    ScreenTrackingService,
+    UserTrackingService, 
+    provideFirestore(() => getFirestore())]
 };
