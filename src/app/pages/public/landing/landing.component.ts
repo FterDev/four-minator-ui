0f1b49cfcbb39593c213dca54c190f318e317751
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-02-06 16:31:28 +0100

    added langding page base

--- src/app/pages/public/landing/landing.component.ts
+++ src/app/pages/public/landing/landing.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'fm-landing',
-  imports: [],
+  imports: [MatCardModule, MatButton, MatToolbarModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
+
+  year = new Date().getFullYear();
 
 }
