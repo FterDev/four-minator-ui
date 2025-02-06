e2f6621294698bfb966f88d750a7257ef4e76e95
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-02-06 16:01:58 +0100

    Added landing page

---
+++ src/app/pages/public/landing/landing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fm-landing',
+  imports: [],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.scss'
+})
+export class LandingComponent {
+
+}
