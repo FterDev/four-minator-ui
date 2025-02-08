6dbeb4e9f6366ad9cd8f93eb44a064d174a772b7
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-02-08 14:16:54 +0100

    Added route to register

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'fm-toolbar',
-  imports: [MatButton, MatToolbarModule],
+  imports: [MatButton, MatToolbarModule, RouterLink],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
