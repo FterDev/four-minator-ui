fdbe8b57c8294efe02bbc1d990bb7d84c305fc45
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-02-08 09:59:25 +0100

    Added basic Header Content

--- src/app/pages/public/landing/landing.component.ts
+++ src/app/pages/public/landing/landing.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { ToolbarComponent } from "../../../components/toolbar/toolbar.component";
+import { HeaderComponent } from "../../../components/header/header.component";
 
 
 @Component({
   selector: 'fm-landing',
-  imports: [MatCardModule, ToolbarComponent, ToolbarComponent],
+  imports: [MatCardModule, ToolbarComponent, ToolbarComponent, HeaderComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
