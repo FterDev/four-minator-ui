c50b2b3df1a5542bf57a1375e45468aa081e1ff4
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-02-08 14:07:44 +0100

    Added basic props to register page

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserRegister } from '../../../interfaces/user-register';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'fm-register',
@@ -7,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  appearance = 'outline';
+
+  user: UserRegister = {
+    email: '',
+    nickname: '',
+    password: '',
+    confirmPassword: '',
+    terms: false
+  }
+
+  registerForm: FormGroup;
+
+  constructor() {
+    this.registerForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      nickname: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl('', [Validators.required]),
+      terms: new FormControl('', [Validators.required]),
+    });
+  }
 
 }
