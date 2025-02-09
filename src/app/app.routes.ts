f2a3e73b34a603da758ffacc5431d872c7597bf3
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-02-09 18:42:47 +0100

    Added auth layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/public/landing/landing.component';
 import { LoginComponent } from './pages/auth/login/login.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
+import { AuthComponent } from './pages/auth/auth.component';
 
 export const routes: Routes = [
     { path: '', component: LandingComponent },
-    { path: 'auth', children: [
+    { path: 'auth', component: AuthComponent, children: [
         { path: 'login', component: LoginComponent },
         { path: 'register', component: RegisterComponent },
       ]
