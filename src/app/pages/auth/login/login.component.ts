e64126d8eb0e71e9c77ce69e6c36e353b6e138b9
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2025-02-16 15:33:25 +0100

    Added base login form

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldAppearance, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
+
+
+type LoginForm = {
+  email: FormControl<string>;
+  password: FormControl<string>;
+}
 
 @Component({
   selector: 'fm-login',
-  imports: [],
+  imports: [ReactiveFormsModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatCardModule, RouterLink],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: '../auth.scss'
 })
 export class LoginComponent {
 
+
+  loginForm: FormGroup;
+  appearance: MatFormFieldAppearance = 'outline';
+
+  constructor() {
+
+    this.loginForm = new FormGroup({
+      email: new FormControl('', []),
+      password: new FormControl('', [])
+    });
+
+    console.log(this.loginForm);
+
+  }
 }
